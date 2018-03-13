5781c13c9c012b8b25963ae2dc627cdc8fb16eb5
Michael Roth <mick2090@gmail.com>
2018-03-13 03:02:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function placeOrder(cardNumber) {
       arrSum += cart[i].itemPrice;
     }
     cart.splice(0, cart.length);
-    return `Your total cost is ${arrSum}, which will be charged to the card ${cardNumber}`;
+    return `Your total cost is $${arrSum}, which will be charged to the card ${cardNumber}`;
   }else{
     return "Sorry we don't have a credit card on file for you."
     

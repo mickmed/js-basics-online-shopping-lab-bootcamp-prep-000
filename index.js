140a10b1d460fbf0dfb3ab4cd47a617abbd6757a
Michael Roth <mick2090@gmail.com>
2018-03-13 02:44:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,11 +47,12 @@ function total() {
 function removeFromCart(item) {
 	for(var i = 0; i<cart.length; i++){
    	if(cart[i].itemName === item){
+    
     	cart.splice(i, 1);
       return cart;
     }
   }
-  return "That item is not in your cart."
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
